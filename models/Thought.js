af315f1a1ad7c4e036931f25f695f373cb832835
otmorales23 <oliverwills0227@gmail.com>
2023-09-11 23:29:40 -0700

    updated thought and user routes, added readme - this app is a work in progress

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
 
-// Schema to create Student model
+// Schema to create Thought model
 const thoughtSchema = new Schema(
   {
     thoughtText: {
